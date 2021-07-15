a4bcba06a807d54fdfdbbc457f973113702ef6e7
luca-bajardi <luca-bajardi@users.github.com>
2021-07-15 16:31:35 +0200

    Update HW1_risk_management.m

--- HW1_risk_management.m
+++ HW1_risk_management.m
@@ -2,7 +2,6 @@
 clc
 clear all
 close all
-format bank
 perc_cover = [0,0.25,0.5,0.75,1];
 %perc_cover = [0:0.001:1];
 perc_forward = [0,0.25,0.5,0.75,1];
@@ -14,14 +13,18 @@ K = 1.22;
 %la politica è stabile su 25000 studenti
 volume_policy = 25000;
 
-impact = hedging_policies(perc_cover, perc_forward, change, volume, volume_policy, K);
+samplesChange = samplingBetaFromData(true);
+
+format bank
+%impact = hedging_policies(perc_cover, perc_forward, change, volume, volume_policy, K);
+impact = hedging_policies(perc_cover, perc_forward, samplesChange, volume, volume_policy, K);
 
 % trovo il minimo lungo la dimensione del volume (dim 1) e lungo quella del
 % change (dim 3) e poi sistemo la matrice per eliminare le dimensioni di 
 % lunghezza 1
 minMatrix = squeeze(min(min(impact,[],1),[],3));
 
-print_table = true;
+print_table = false;
 if print_table
     disp(array2table(minMatrix,...
                       'VariableNames',cellstr(num2str(perc_forward')),...
