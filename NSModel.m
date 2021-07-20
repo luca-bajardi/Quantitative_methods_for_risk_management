352507d07fab67961ae6a24df8f2529f265d93fb
FrancescaCollini <francesca.collini@hotmail.it>
2021-07-20 12:13:35 +0200

    MartelliniHedging

---
+++ NSModel.m
@@ -0,0 +1,72 @@
+%abbiamo 3 diversi fattori di rischio che sono collegati
+%perchè sono i tassi alle diverse maturità quindi non possono essere
+%simulati indipendentemente.
+%in t=0 diamo i parametri della curva
+clc
+clear all
+close all
+t_0=0;
+
+%COSA CAMBIA AL VARIARE dei BETA??
+beta_0=0.08;
+beta_1=-0.03;
+beta_2=-0.01;
+tau_1=3;
+r= @(T) beta_0+beta_1*((1-exp(-T/tau_1))/(T/tau_1))...
+    +beta_2*((1-exp(-T/tau_1))/(T/tau_1)-exp(-T/tau_1));
+%consideriamo 3 bond diversi ===> 3 fattori di rischio
+b1=[2*12, 100, 0.05,1];
+b2=[7*12, 100, 0.05, 1];
+b3=[15*12, 100, 0.05, 1];
+B1=[1, 1, 2*12, 100, 0.05, 1];
+B2=[1, 1, 7*12, 100, 0.05, 1];
+B3=[1, 1, 15*12, 100, 0.05, 1];
+portfolio=[3, 1, 1, 1, b1, b2, b3];
+%[price1, D01, D11, D21]=fixed_duration(t_0, B1, r, 3);
+%[price2, D02, D12, D22]=fixed_duration(t_0, B2, r, 3);
+%[price3, D03, D13, D23]=fixed_duration(true, t_0, B3, r, 3);
+[priceP, D0P, D1P, D2P]=fixed_duration(true, t_0, portfolio, r, 3);
+
+
+
+%Svensson ===> 4 fattori di rischio
+r2= @(T) beta_0+beta_1*((1-exp(-T/tau_1))/(T/tau_1))...
+    +beta_2*((1-exp(-T/tau_1))/(T/tau_1)-exp(-T/tau_1))...
+    +beta_3*((1-exp(-T/tau_2))/(T/tau_2)-exp(-T/tau_2));
+
+
+%%
+%Vasicek=esempio 6.8
+clear all 
+close all
+clc
+
+L=0.06;
+S=0.025;
+G=-0.05;
+a=0.4;
+%syms L S G T
+rv=@(T) L-S*((1-exp(-0.4*T))/(0.4*T))+G*((1-exp(-0.4*T))^2/(4*0.4*T));
+%rv=subs(rv, {L, S, G, T}, {0.06, 0.025, -0.05, sym('T')});
+%rv=subs(rv, {L, S, G, T}, {sym('L'), sym('S'), sym('G'), 1});
+
+%beta_0=L;
+%beta_1=-S;
+%beta_2= @(T) (G/4+G/4*exp(-2*a*T)-G/2*exp(-a*T))/(1-(exp(-a*T)*(1+a*T)));
+%tau_1=1/a;
+%r= @(T) beta_0+beta_1*((1-exp(-T/tau_1))/(T/tau_1))...
+%    +beta_2(T)*((1-exp(-T/tau_1))/(T/tau_1)-exp(-T/tau_1));
+%posso calcolarli con la funzione di prima
+%P=32863500;
+%DL=-224016404;
+%DS=63538154;
+%DG=-13264994;
+
+%treasury bonds as hedging assets
+A1=[3*12, 100, 0.07, 1];
+A2=[7*12, 100, 0.08, 1];
+A3=[12*12, 100, 0.05, 1];
+A4=[18*12, 100, 0.06, 1];
+
+
+[p, Dl, Ds,Dg ]=fixed_duration(false, 0, [1, 1, A1], rv, 1/a);
---
+++ fixed_duration.m
@@ -0,0 +1,60 @@
+function [price, D_0, D_1,D_2] = fixed_duration(NS, t_0, portfolio, r, tau_1)
+I=floor(portfolio(1));
+%I=length(portfolio(n_elements+2:length(portfolio)))/4;
+N=portfolio(2:(2+I-1));
+prices=zeros(1, I);
+d_0=zeros(1, I);
+d_1=zeros(1, I);
+d_2=zeros(1, I);
+%d_3=zeros(1, I);
+
+for i=0:(I-1)
+T=portfolio(I+2+4*i);
+faceValue=portfolio(I+3+4*i);
+annualCoupon=portfolio(I+4+4*i);
+NPayments=portfolio(I+5+4*i);
+
+
+c=annualCoupon/NPayments*faceValue;
+if mod(T, 12/NPayments)==0
+    n=NPayments*T/12-1; %numero di pagamenti escluso quello della maturità
+    time=linspace(t_0, T/12, n+2);
+    time(1)=[];
+    
+else
+    t_1=mod(T, 12/NPayments);
+    n=floor(NPayments*T/12);
+    time=linspace(t_1/12, T/12, n+1);
+    
+end 
+termStructure=zeros(1, length(time));
+for j=1:length(time)
+termStructure(1, j)=r(time(j));
+
+end
+t=time.*termStructure;
+prices(1, i+1)=c*sum(exp(-(t(1:n)-t_0)))+(c+faceValue)*exp(-(t(n+1)-t_0));
+%D_0=diff(prices(1, i+1), L);
+%su=subs(D_0, {L, S, G, T}, {l, s, g, 36});
+
+d_0(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0)))+(time(n+1)*(c+faceValue)*exp(-(t(n+1)-t_0))));
+if NS==true
+d_1(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*(1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)))...
+    +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*(1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)));
+d_2(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)-exp(-time(1:n)/tau_1))))...
+    +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)-exp(-time(n+1)/tau_1))));
+%d_3(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_2))./(time(1:n)/tau_2)-exp(-time(1:n)/tau_2))))...
+%    +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_2))./(time(n+1)/tau_2)-exp(-time(n+1)/tau_2))));
+else
+    d_1(1, i+1)=((c*sum(time(1:n).*exp(-t(1:n)-t_0).*(1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)))...
+    +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*(1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)));
+    d_2(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_1)).^2./(4*time(1:n)/tau_1))))...
+    +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_1)).^2./(4*time(n+1)/tau_1))));
+end
+end 
+   price=sum(N.*prices); %valore del bond
+D_0=sum(N.*d_0); %dollar duration
+D_1=sum(N.*d_1);
+D_2=sum(N.*d_2);
+
+end
