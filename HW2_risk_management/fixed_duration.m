bc901783571b91575505b98ec44f6aa0b6307a81
luca-bajardi <luca-bajardi@users.github.com>
2021-07-25 21:10:59 +0200

    Update fixed_duration.m

--- HW2_risk_management/fixed_duration.m
+++ HW2_risk_management/fixed_duration.m
@@ -1,4 +1,12 @@
-function [price, D_0, D_1, D_2, D_3] = fixed_duration(NS, t_0, portfolio, r, tau, betaValues)
+function [price, D_0, D_1, D_2, D_3] = fixed_duration(t_0, portfolio, r, tau, betaValues)
+%DETTAGLI PORTAFOGLIO
+%quanti bond nel portafoglio
+%quanti bond di quel tipo
+%maturità in mesi
+%face value
+%cedola annuale
+%numero capitalizzazioni in un anno
+
 % r è la formula della struttura per scadenza
 % tau_1 non sappiamo come si trova
 I=floor(portfolio(1));
@@ -10,7 +18,8 @@ d_1=zeros(1, I);
 d_2=zeros(1, I);
 d_3=zeros(1, I);
 tau_1 = tau(1);
-if length(tau)==2
+%disp(betaValues)
+if length(tau)~=1 && betaValues(4)~=0
     tau_2 = tau(2);
 end
 
@@ -35,11 +44,7 @@ for i=0:(I-1)
     end
     termStructure=zeros(1, length(time));
     for j=1:length(time)
-        if NS==true
-            termStructure(1, j)=r(betaValues,tau,time(j));
-        else
-            termStructure(1, j)=r(time(j));
-        end
+        termStructure(1, j)=r(betaValues,tau,time(j));
         
     end
     t=time.*termStructure;
@@ -49,21 +54,15 @@ for i=0:(I-1)
     
     %derivata parziale del prezzo rispetto beta_0
     d_0(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0)))+(time(n+1)*(c+faceValue)*exp(-(t(n+1)-t_0))));
-    if NS==true
-        d_1(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*(1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)))...
-            +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*(1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)));
-        d_2(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)-exp(-time(1:n)/tau_1))))...
-            +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)-exp(-time(n+1)/tau_1))));
-        if length(tau)==2
+    d_1(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*(1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)))...
+        +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*(1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)));
+    d_2(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)-exp(-time(1:n)/tau_1))))...
+        +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)-exp(-time(n+1)/tau_1))));
+    if length(tau)~=1 && betaValues(4)~=0
         d_3(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_2))./(time(1:n)/tau_2)-exp(-time(1:n)/tau_2))))...
             +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_2))./(time(n+1)/tau_2)-exp(-time(n+1)/tau_2))));
-        end
-    else
-        d_1(1, i+1)=((c*sum(time(1:n).*exp(-t(1:n)-t_0).*(1-exp(-time(1:n)/tau_1))./(time(1:n)/tau_1)))...
-            +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*(1-exp(-time(n+1)/tau_1))./(time(n+1)/tau_1)));
-        d_2(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0).*((1-exp(-time(1:n)/tau_1)).^2./(4*time(1:n)/tau_1))))...
-            +((c+faceValue)*time(n+1).*exp(-t(n+1)-t_0).*((1-exp(-time(n+1)/tau_1)).^2./(4*time(n+1)/tau_1))));
     end
+    
 end
 price=sum(N.*prices); %valore del bond
 
@@ -71,7 +70,7 @@ price=sum(N.*prices); %valore del bond
 D_0=sum(N.*d_0); %dollar duration
 D_1=sum(N.*d_1);
 D_2=sum(N.*d_2);
-if length(tau)==2
+if length(tau)~=1 && betaValues(4)~=0
     D_3=sum(N.*d_3);
 else
     D_3=NaN;
