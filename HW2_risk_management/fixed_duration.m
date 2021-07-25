1edf4cace1c381d924565bec5941262fbb7d09f0
luca-bajardi <luca-bajardi@users.github.com>
2021-07-25 22:10:16 +0200

    Update fixed_duration.m

--- HW2_risk_management/fixed_duration.m
+++ HW2_risk_management/fixed_duration.m
@@ -9,8 +9,8 @@ function [price, D_0, D_1, D_2, D_3] = fixed_duration(t_0, portfolio, r, tau, be
 
 % r è la formula della struttura per scadenza
 % tau_1 non sappiamo come si trova
+
 I=floor(portfolio(1));
-%I=length(portfolio(n_elements+2:length(portfolio)))/4;
 N=portfolio(2:(2+I-1));
 prices=zeros(1, I);
 d_0=zeros(1, I);
@@ -18,7 +18,7 @@ d_1=zeros(1, I);
 d_2=zeros(1, I);
 d_3=zeros(1, I);
 tau_1 = tau(1);
-%disp(betaValues)
+
 if length(tau)~=1 && betaValues(4)~=0
     tau_2 = tau(2);
 end
@@ -35,12 +35,10 @@ for i=0:(I-1)
         n=NPayments*T/12-1; %numero di pagamenti escluso quello della maturità
         time=linspace(t_0, T/12, n+2);
         time(1)=[];
-        
     else
         t_1=mod(T, 12/NPayments);
         n=floor(NPayments*T/12);
         time=linspace(t_1/12, T/12, n+1);
-        
     end
     termStructure=zeros(1, length(time));
     for j=1:length(time)
@@ -49,8 +47,6 @@ for i=0:(I-1)
     end
     t=time.*termStructure;
     prices(1, i+1)=c*sum(exp(-(t(1:n)-t_0)))+(c+faceValue)*exp(-(t(n+1)-t_0));
-    %D_0=diff(prices(1, i+1), L);
-    %su=subs(D_0, {L, S, G, T}, {l, s, g, 36});
     
     %derivata parziale del prezzo rispetto beta_0
     d_0(1, i+1)=-((c*sum(time(1:n).*exp(-t(1:n)-t_0)))+(time(n+1)*(c+faceValue)*exp(-(t(n+1)-t_0))));
