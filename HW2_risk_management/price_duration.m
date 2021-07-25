25d7db43c0d34788636f2e2841fcfdf166225966
FrancescaCollini <francesca.collini@hotmail.it>
2021-07-25 19:54:39 +0200

    Swap

--- HW2_risk_management/price_duration.m
+++ HW2_risk_management/price_duration.m
@@ -1,4 +1,4 @@
-function [price, D, dollar_duration, C, dollar_convexity] = price_duration(t_0, portfolio,termStructure, last_rate)
+function [price, D, dollar_duration] = price_duration(t_0, portfolio, tau, betaValues, last_rate)
 %Argomenti in input: 
 %t_0=istante di tempo in cui voglio calcolare il valore del portafoglio
 %portfolio=[NDifferentBond, NBond, T, faceValue, annualCoupon, NPayments], portafoglio da immunizzare
@@ -10,58 +10,67 @@ function [price, D, dollar_duration, C, dollar_convexity] = price_duration(t_0, 
 %price=valore attuale del portafoglio
 %D=duration
 %dollar_duration
-%C=Convexity
-%dollar_convexity
 
-n_elements=floor(portfolio(1));
-I=length(portfolio(n_elements+2:length(portfolio)))/4;
-N=portfolio(2:(2+n_elements-1));
+
+I=floor(portfolio(1));
+N=portfolio(2:(2+I-1));
 prices=zeros(1, I);
 durations=zeros(1, I);
-convexities=zeros(1, I);
 
 for i=0:(I-1)
-T=portfolio(n_elements+2+4*i);
-faceValue=portfolio(n_elements+3+4*i);
-annualCoupon=portfolio(n_elements+4+4*i);
-NPayments=portfolio(n_elements+5+4*i);
+    T=portfolio(I+2+4*i);
+    faceValue=portfolio(I+3+4*i);
+    annualCoupon=portfolio(I+4+4*i);
+    NPayments=portfolio(I+5+4*i);
 
     if ~exist('last_rate','var') %il portafoglio è costituito da bond
-        c=annualCoupon/NPayments*faceValue;
-        if mod(T, 12/NPayments)==0
-            n=NPayments*T/12-1; %numero di pagamenti escluso quella della maturità
-            time=linspace(t_0, T/12, n+2);
-            time(1)=[];
-            t=time.*termStructure(1:(NPayments*T/12));
-        else
-            t_1=mod(T, 12/NPayments);
-            n=floor(NPayments*T/12);
-            time=linspace(t_1/12, T/12, n+1);
-            t=time.*termStructure(1:length(time));
-        end 
-        prices(1, i+1)=c*sum(exp(-(t(1:n)-t_0)))+(c+faceValue)*exp(-(t(n+1)-t_0));
-        durations(1, i+1)=1/prices(1, i+1)*(sum(c*(time(1:n)-t_0).*exp(-(t(1:n)-t_0)))+(c+faceValue)*(time(n+1)-t_0)*exp(-(t(n+1)-t_0)));
-        convexities(1, i+1)=1/prices(1, i+1)*(sum(c*(time(1:n)-t_0).^2.*exp(-(t(1:n)-t_0)))+(c+faceValue)*(time(n+1)-t_0)^2*exp(-(t(n+1)-t_0)));
+        if mod(T, 1/NPayments)==0
+        n=NPayments*T-1; %numero di pagamenti escluso quello della maturità
+        time=linspace(t_0, T, n+2);
+        time(1)=[];
+        
+    else
+        t_1=mod(T, 1/NPayments);
+        n=floor(NPayments*T);
+        time=linspace(t_1, T, n+1);
+        
+    end
+   termStructure=zeros(1, length(time));
+   for j=1:length(time)
+      %  if NS==true
+           termStructure(1, j)=r(betaValues,tau,time(j));
+       % else
+        %    termStructure(1, j)=r(time(j));
+   end
+        
+   % end
+    t=time.*termStructure;
+    if ~isnan(annualCoupon)
+         c=annualCoupon/NPayments*faceValue;
+     else    %calcoliamo lo swap rate 
+        c=NPayments*(1-exp(-T*termStructure(T+2)))/sum(exp(-t(1:T+2)));
+    end
+    prices(1, i+1)=c*sum(exp(-(t(1:n)-t_0)))+(c+faceValue)*exp(-(t(n+1)-t_0));
+    durations(1, i+1)=1/prices(1, i+1)*(sum(c*(time(1:n)-t_0).*exp(-(t(1:n)-t_0)))+(c+faceValue)*(time(n+1)-t_0)*exp(-(t(n+1)-t_0)));
+       
 
     else          %il portafoglio è costituito da floater
-        if mod(T,12/NPayments)==0
-            next_reset=12/NPayments;
+        if mod(T,1/NPayments)==0
+            next_reset=1/NPayments;
         else
-            next_reset=mod(T,12/NPayments);
+            next_reset=mod(T,1/NPayments);
         end
     c=faceValue*last_rate/NPayments;
-    prices(1, i+1)=(c+faceValue)*exp(-(next_reset/12-t_0)*termStructure(1));
-    durations(1, i+1)=(next_reset-t_0)/12;
+    prices(1, i+1)=(c+faceValue)*exp(-(next_reset-t_0)*termStructure(1));
+    durations(1, i+1)=(next_reset-t_0);
     end
 
 price=sum(N.*prices);
 D=1/price*sum(N.*prices.*durations);
 dollar_duration=D*price;
-C=1/price*(sum(N.*prices.*convexities));
-dollar_convexity=price*C;
-
-
-    
+%C=1/price*(sum(N.*prices.*convexities));
+%dollar_convexity=price*C;
+ 
     
 end    
 end
