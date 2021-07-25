66a309d48d1f43da7e831be177dc320f891cec0a
luca-bajardi <luca-bajardi@users.github.com>
2021-07-25 16:25:01 +0200

    Reorder file

--- HW2_risk_management/HW2_risk_management.m
+++ HW2_risk_management/HW2_risk_management.m
@@ -1,12 +1,17 @@
 clear all
 close all
 clc
-
+format bank
 
 termStructure=[0.03, 0.03, 0.03];  %%attuale
 t_0=0;
 portfolio=[1, 1, 18, 10000, 0.05, 2]; %Porfolio da immunizzare
-
+%quanti bond nel portafoglio
+%quanti bond di quel tipo
+%maturità in mesi
+%face value
+%cedola annuale
+%numero capitalizzazioni in un anno
 
 %DURATION MATCHING
 
