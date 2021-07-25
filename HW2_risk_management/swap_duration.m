0275ed6d03b0a69ab065f3b44a02baffac03dd9c
luca-bajardi <luca-bajardi@users.github.com>
2021-07-25 22:12:31 +0200

    Update swap_duration.m

--- HW2_risk_management/swap_duration.m
+++ HW2_risk_management/swap_duration.m
@@ -1,4 +1,4 @@
-function [p_swap,Dd_swap] = swap_duration(fixed, t_0, T, faceValue, NPayments, tau, betaValues, last_rate)
+function [p_swap,Dd_swap] = swap_duration(fixed, t_0, T, faceValue, NPayments, r, tau, betaValues, last_rate)
 
 %Argomenti in input: 
 %fixed=variabile booleana per stabilire il punto di vista fixed/floater
@@ -8,8 +8,8 @@ function [p_swap,Dd_swap] = swap_duration(fixed, t_0, T, faceValue, NPayments, t
 %last_rate= per calcolare il prossimo pagamento del floater
 
 portfolio=[1, 1, T, faceValue, NaN, NPayments];
-[p_fixed, ~, Dd_fixed]=price_duration(t_0, portfolio, tau, betaValues);
-[p_float, ~, Dd_float]=price_duration(t_0,portfolio, tau, betaValues, last_rate);
+[p_fixed, ~, Dd_fixed]=price_duration(t_0, portfolio, r, tau, betaValues);
+[p_float, ~, Dd_float]=price_duration(t_0, portfolio, r, tau, betaValues, last_rate);
 
 if fixed==true                          %dal punto di chi paga fisso
     p_swap=p_float-p_fixed;
