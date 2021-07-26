2b781d93e4da2b16f9369c18fd35fac06ebde95b
luca-bajardi <luca-bajardi@users.github.com>
2021-07-26 16:07:32 +0200

    Update portfolioShock.m

--- HW2_risk_management/portfolioShock.m
+++ HW2_risk_management/portfolioShock.m
@@ -1,11 +1,22 @@
-function [newValuePosition,priceBondOriginal,priceBondNew] = portfolioShock(t_0,bond,hedge,r,tau,betaValues,betaVariation,phi)
+function [newValuePosition,priceBondOriginal,priceBondNew] = portfolioShock(t_0,bond,hedge,r,tau,betaValues,betaVariation,phi,type,fixed,last_rate)
 
-hedge(2:2+hedge(1)-1)=phi;
 
-[priceBondOriginal, D0bond, D1bond, D2bond]=fixed_duration(t_0, bond, r, tau, betaValues);
-[priceBondNew, D0bond, D1bond, D2bond]=fixed_duration(t_0, bond, r, tau, betaValues+betaVariation);
 
-[priceZeroOriginal, D0zero, D1zero, D2zero]=fixed_duration(t_0, hedge, r, tau, betaValues);
-[priceZero, D0zero, D1zero, D2zero]=fixed_duration(t_0, hedge, r, tau, betaValues+betaVariation);
+[priceBondOriginal, ~, ~, ~]=fixed_duration(t_0, bond, r, tau, betaValues);
+[priceBondNew, ~, ~, ~]=fixed_duration(t_0, bond, r, tau, betaValues+betaVariation);
 
-newValuePosition = priceBondNew + (priceZero - priceZeroOriginal);
+if exist('type','var') && strcmp(type,'swap')
+    [priceSwapOriginal, ~,k,last_rateUsed]=swap_duration(t_0, hedge, r, tau, betaValues, fixed, last_rate);
+    I=floor(hedge(1));
+    for i=0:(I-1)
+        hedge(I+4+4*i) = k; %diciamo qual è l'annual coupon perché già definito e non influenzato dagli shock
+    end
+    [priceSwap, ~, ~, ~]=swap_duration(t_0, hedge, r, tau, betaValues+betaVariation, fixed, last_rateUsed);
+    newValuePosition = priceBondNew + (priceSwap - priceSwapOriginal);
+else
+    hedge(2:2+hedge(1)-1)=phi;
+    [priceZeroOriginal, ~, ~, ~]=fixed_duration(t_0, hedge, r, tau, betaValues);
+    [priceZero, ~, ~, ~]=fixed_duration(t_0, hedge, r, tau, betaValues+betaVariation);
+    newValuePosition = priceBondNew + (priceZero - priceZeroOriginal);
+end
+
