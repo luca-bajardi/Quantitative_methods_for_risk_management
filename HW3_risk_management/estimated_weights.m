dd08520a196c8a51da5587132400001ecea8c0f3
luca-bajardi <luca-bajardi@users.github.com>
2021-07-26 22:50:30 +0200

    Create estimated_weights.m

---
+++ HW3_risk_management/estimated_weights.m
@@ -0,0 +1,20 @@
+function w = estimated_weights(numRepl,numSample, trueMu, trueSigma, lambda)
+
+rng('default'); % ripetibilità
+c=length(trueMu);
+w = zeros(numRepl, c);
+%w1=zeros(numRepl,1);
+% matrice per i pesi di ciascun asset nel portafoglio ottimo stimato
+    for k=1:numRepl
+        retScenarios = mvnrnd(trueMu,trueSigma,numSample); %estrae vettori casuali dalla distribuzione normale
+        hatMu = mean(retScenarios);
+        hatSigma = cov(retScenarios);
+        wp = QuadFolio(hatMu, hatSigma, lambda);
+
+        for i=1:c
+            w(k, i) = wp(i);
+        end
+    end
+end
+
+
