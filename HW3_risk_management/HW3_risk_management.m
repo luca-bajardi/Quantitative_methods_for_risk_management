f2f36990c666f5f06ae2b754e3a8425db0da0b99
luca-bajardi <luca-bajardi@users.github.com>
2021-07-27 15:23:31 +0200

    Update HW3_risk_management.m

--- HW3_risk_management/HW3_risk_management.m
+++ HW3_risk_management/HW3_risk_management.m
@@ -1,6 +1,6 @@
+clc
 clear all
 close all
-clc
 %Sto usando questo tipo di modello (assumo una distribuzione di probabilità),
 %ma non vado a beccare i parametri giusti
 %quindi si introduce del rumore nel portafoglio. Vediamo cosa comporta in termini
@@ -71,7 +71,7 @@ trueSigma = cov(returns')
 % trueSigma = [1 trueRho; trueRho 1].*(trueVols * trueVols');
 lambda = 3;
 %Non impongo vincoli sulla vendita allo scoperto
-[truewp,mup,sigmap] = QuadFolio(trueMu, trueSigma, lambda);
+[truewp,~,~] = QuadFolio(trueMu, trueSigma, lambda);
 truew1 = truewp(1);
 
 figure
@@ -101,7 +101,14 @@ max(abs(truewp-x))<1.0e-4
 %facendo varie replicazioni vediamo che a seconda dei valori di mu e sigma
 %campionati si ottengono valori di w diversi (usando 250 campioni ad
 %indicare un'analisi relativa all'anno precedente
-w=estimated_weights(1000, 250, trueMu, trueSigma, lambda);
+% w=estimated_weights(1000, 250, trueMu, trueSigma, lambda);
+% figure
+% histogram(w(:,1),50); %peso del primo asset nel portafoglio
+% xline(truew1,'r','LineWidth',3); %valore ottimo vero
+% title('Peso del primo asset nel portafoglio');
+
+rng('default'); % ripetibilità
+[~,w] = estimated_portfolio(1000, trueMu, trueSigma, lambda, 0, 'estimated', false, 250);
 figure
 histogram(w(:,1),50); %peso del primo asset nel portafoglio
 xline(truew1,'r','LineWidth',3); %valore ottimo vero
@@ -120,7 +127,9 @@ title('Peso del primo asset nel portafoglio');
 %vediamo come migliora la soluzione quando aumentiamo il numero di
 %campioni a disposizione
 figure 
-w1=estimated_weights(1000, 2500, trueMu, trueSigma, lambda);
+% w1=estimated_weights(1000, 2500, trueMu, trueSigma, lambda);
+rng('default'); % ripetibilità
+[~,w1] = estimated_portfolio(1000, trueMu, trueSigma, lambda, 0, 'estimated', false, 2500);
 histogram(w1(:, 1),50);
 xline(truew1,'r','LineWidth',3);
 title('Aumento la dimensione dei dati su cui fitto la distribuzione')
@@ -131,7 +140,7 @@ title('Aumento la dimensione dei dati su cui fitto la distribuzione')
 %%
 %EFFETTO DELL'AVVERSIONE AL RISCHIO
  lambda2=2;
-[truewp,mup,sigmap] = QuadFolio(trueMu, trueSigma, lambda2);
+[truewp,~,~] = QuadFolio(trueMu, trueSigma, lambda2);
 Weights(:,1) = truewp;
 % truew_lambda = truewp(1);
 % w_lambda=estimated_portfolio(1000, 300, trueMu, trueSigma, lambda2);
@@ -141,7 +150,7 @@ Weights(:,1) = truewp;
 % title('Effetto del coef. di avversione al rischio');
 
 lambda3=4;
-[truewp,mup,sigmap] = QuadFolio(trueMu, trueSigma, lambda3);
+[truewp,~,~] = QuadFolio(trueMu, trueSigma, lambda3);
 % truew_lambda = truewp(1);
 Weights(:,3) = truewp;
 % w_lambda=estimated_portfolio(1000, 300, trueMu, trueSigma, lambda3);
@@ -163,14 +172,16 @@ bar(stockCat,Weights)
 %Non solo richiediamo che la somma di tutti i pesi deve essere uguale ad 1,
 %ma anche che tutti i pesi devono essere positivi (no vendita allo
 %scoperto)
-options = optimoptions('quadprog','Display','none');
-x_no_scoperto = quadprog(lambda*trueSigma,-trueMu,-eye(length(trueMu)),zeros(length(trueMu),1),ones(1,length(trueMu)),1,[],[],[],options);
+% options = optimoptions('quadprog','Display','none');
+% rng('default');
+% x_no_scoperto1 = quadprog(lambda*trueSigma,-trueMu,-eye(length(trueMu)),zeros(length(trueMu),1),ones(1,length(trueMu)),1,[],[],[],options);
+rng('default');
+[~,x_no_scoperto] = estimated_portfolio(1, trueMu, trueSigma, lambda, 0, 'optimalNoShort', false, 0);
 % wns=estimated_portfolio(1000, 3000, trueMu, trueSigma, lambda);
 % figure
 % histogram(wns(:, 1),50);
 % xline(x_no_scoperto(1),'r','LineWidth',3);
 % title('No vendita allo scoperto');
-figure
 bar(stockCat,x_no_scoperto)
 title('Weights with w>=0')
 
@@ -180,8 +191,14 @@ title('Weights with w>=0')
 %scelta azzardata, ma visto che sto commettendo degli errori di stima
 %faccio entrare meno errori nel modello, quindi in alcune circostanze
 %potrebbe essere utile capire cosa accade.
-options = optimoptions('quadprog','Display','none');
-xmv= quadprog(lambda*trueSigma,[],[],[],ones(1,num_asset),1,[],[],[],options);
+% options = optimoptions('quadprog','Display','none');
+% rng('default'); 
+% returnsObs = mvnrnd(trueMu,trueSigma,250);
+% hatSigma = cov(returnsObs);
+% xmv= quadprog(lambda*hatSigma,[],[],[],ones(1,num_asset),1,[],[],[],options);
+rng('default'); % ripetibilità
+[~,xmv] = estimated_portfolio(1, trueMu, trueSigma, lambda, 0, 'minvariance', false, 250);
+
 % truew_lambda = xmv(2);
 % %non so se serve farlo??
 % w_mv=minvariance(1000, 300, trueMu, trueSigma, lambda, options);
@@ -189,6 +206,9 @@ xmv= quadprog(lambda*trueSigma,[],[],[],ones(1,num_asset),1,[],[],[],options);
 % histogram(w_mv(:,2),50); %peso del secondo asset nel portafoglio
 % xline(truew_lambda,'r','LineWidth',3); %valore ottimo vero
 % title('Portafoglio di minima varianza');
+% figure
+% bar(stockCat,xmv)
+% title('Pesi di un portafoglio di minima varianza')
 figure
 bar(stockCat,xmv)
 title('Pesi di un portafoglio di minima varianza')
@@ -200,25 +220,57 @@ title('Pesi di un portafoglio di minima varianza')
 %PORTAFOGLIO NAIVE
 %Stesso orizzonte di tempo di prima, confrontare il rendimento
 w_naive=1/length(trueMu)*ones(length(trueMu), 1);
-% figure
-% bar(stockCat,w_naive)
-% title('Pesi di un portafoglio naive')
+figure
+bar(stockCat,w_naive)
+title('Pesi di un portafoglio naive')
 %[x0,y0,width,height] = [0,0,0.2,0.2];
 % fig=gcf;
 % fig.Position(2)=0;
 % fig.Position(4)=0.2;
-
+ylim([-0.05 0.2])
 %% stima rendimenti
+rng(2020); % ripetibilità
+numRepl = 1;
+plotWealth = true;
 num_days = 250;
-
+numPastDays = 250;
 types = ["optimal","optimalNoShort","estimated","estimatedNoShort","minvariance","naive"];
 for type = 1:length(types)
-    estimated_portfolio(trueMu, trueSigma, lambda, num_days, types(type));
+    estimated_portfolio(numRepl, trueMu, trueSigma, lambda, num_days, types(type), plotWealth, numPastDays);
+end
+
+%% stima rendimenti con aggiornamento pesi ogni settimana
+rng(2020); % ripetibilità
+plotWealth = true;
+num_days = 250;
+num_days_update = 5;
+types = ["estimated","estimatedNoShort","minvariance"];
+for type = 1:length(types)
+    estimated_portfolio(numRepl, trueMu, trueSigma, lambda, num_days, types(type), plotWealth, numPastDays, num_days_update);
 end
 
 %%
 %Hanno più impatto errori sui premi per il rischio o sulle
 %covarianze/correlazioni?? errori sui premi per il rischio, PERCHE'????
+numRepl = 1000;
+plotWealth = false;
+rng(2020);
+wealth = estimated_portfolio(numRepl, trueMu, trueSigma, lambda, num_days, 'estimated', plotWealth, numPastDays, num_days_update);
+figure
+hist_values = wealth(:,end)<50000;
+histogram(wealth(hist_values,end),30)
+
+%applichiamo piccola variazione sulla stima (errore di stima) e aumenta
+%variabilità
+muVariation = zeros(length(trueMu),1);
+muVariation(5) =  0.0001;
+muVariation(10) = -0.0001;
+
+wealth = estimated_portfolio(numRepl, trueMu+muVariation, trueSigma, lambda, num_days, 'estimated', plotWealth, numPastDays, num_days_update);
+
+figure
+hist_values = wealth(:,end)<50000;
+histogram(wealth(hist_values,end),30)
 
 %%
 %Effetto di asimmetrie o code grasse (volendo potete cambiare distribuzione di probabilità e
