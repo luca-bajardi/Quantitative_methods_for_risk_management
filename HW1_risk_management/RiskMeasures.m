9c36c099527be0e9ec99b36144da22a2d8743bbe
luca-bajardi <luca-bajardi@users.github.com>
2021-07-27 23:57:37 +0200

    Update RiskMeasures.m

--- HW1_risk_management/RiskMeasures.m
+++ HW1_risk_management/RiskMeasures.m
@@ -10,10 +10,6 @@ sigma = zeros(num_perc_cover, num_perc_forward);
 VaR = zeros(num_prob,num_perc_cover, num_perc_forward);
 CVaR = zeros(num_prob,num_perc_cover, num_perc_forward);
 
-%%OTTIMIZZAZIONE ROBUSTA
-% trovo il minimo lungo la dimensione del volume (dim 1) e lungo quella del
-% change (dim 3) e poi sistemo la matrice per eliminare le dimensioni di 
-% lunghezza 1
 minMatrix = squeeze(min(min(impact,[],1),[],3));
 for i = 1:num_perc_cover
     for j = 1:num_perc_forward %per ogni strategia
